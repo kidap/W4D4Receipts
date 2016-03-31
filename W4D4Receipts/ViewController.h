1789456aa61ff177997f757e17e48386ad59cefd
Karlo Pagtakhan <kidap@yahoo.com>
2016-03-31 11:16:18 -0400

    Initial commit

---
+++ W4D4Receipts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  W4D4Receipts
+//
+//  Created by Karlo Pagtakhan on 03/31/2016.
+//  Copyright © 2016 AccessIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
